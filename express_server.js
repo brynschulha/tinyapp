128f67931978660074cbcecedbe042bacefe0328
Bryn <bschulha@ualberta.ca>
2020-09-29 03:31:42 +0000

    Added the logic to the generateRandomString function

--- express_server.js
+++ express_server.js
@@ -4,8 +4,16 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
 function generateRandomString() {
-
-}
+  let possibleCharacters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, w, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let randomString = [];
+  let count = 1;
+  while (count < 7) {
+    randomString.push(possibleCharacters[(Math.floor(Math.random() * 36))]);
+    count += 1;
+  }
+  let finalString = randomString.join("");
+  return finalString;
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 
