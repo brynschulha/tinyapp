e8dbc2153a8585cc2d4f024ff7889bd0f3cbafa0
Bryn <bschulha@ualberta.ca>
2020-09-30 08:53:51 +0000

    Updated existing routes to display the username

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 const generateRandomString = function () {
   let possibleCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "w", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -16,6 +17,8 @@ const generateRandomString = function () {
 };
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -37,12 +40,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};  
+  res.render("urls_index", templateVars);     
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);   
 });
 
 app.post("/urls", (req, res) => {
@@ -64,14 +68,13 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body.username);
   //set a cookie to username
   res.cookie("username", req.body.username);
   res.redirect(`/urls`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };  
   res.render("urls_show", templateVars);
 });
 
